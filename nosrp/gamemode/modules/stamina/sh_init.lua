972b76001f6fb8bb63cca96a0142ba3b1715dbe3
Jakob Müller <j.mueller@outlook.de>
2016-11-12 22:58:46 +0100

    Bug Fix

--- nosrp/gamemode/modules/stamina/sh_init.lua
+++ nosrp/gamemode/modules/stamina/sh_init.lua
@@ -4,83 +4,83 @@ Stamina.LastSound = CurTime()
 
 if SERVER then
 	function PLAYER_META:IsRunning()
-		   return self:KeyDown( IN_SPEED ) and (self:GetVelocity():Length() > 2 && not self:InVehicle())
+		return self:KeyDown( IN_SPEED ) and (self:GetVelocity():Length() > 2 && not self:InVehicle())
 	end
 
 	function PLAYER_META:StaminaCooldown( bool )
-		   self.staminacooldown = bool
+		self.staminacooldown = bool
 	end
 
 	function PLAYER_META:HasStaminaCooldown()
-		   return self.staminacooldown
+		return self.staminacooldown
 	end
 
 	function Stamina.GetStamina( ply )
-		   return math.Round( math.min( ply:GetNWInt( "stamina" ) ) )
+		return math.Round( math.min( ply:GetNWInt( "stamina" ) ) )
 	end
 
 	function Stamina.PlayerInitialize( ply )
-		   if IsValid( ply ) then
-			   ply:SetNWInt( "stamina", 100 )
-			   ply.defaultrunspeed = ply:GetRunSpeed()
-			   timer.Create( ply:UniqueID() .. "_staminacheck", SETTINGS.STAMINA_REFILL_SPEED, 0, function() Stamina.Check( ply ) end )
-		   end
+	   if IsValid( ply ) then
+		   ply:SetNWInt( "stamina", 100 )
+		   ply.defaultrunspeed = ply:GetRunSpeed()
+		   timer.Create( ply:UniqueID() .. "_staminacheck", SETTINGS.STAMINA_REFILL_SPEED, 0, function() Stamina.Check( ply ) end )
+	   end
 	end
 	hook.Add( "PlayerInitialSpawn", "NOS Stamina Timer", Stamina.PlayerInitialize )
 
 	function Stamina.Check( ply )
-		   if IsValid( ply ) then
-			   ply:SendLua('Stamina.PlayExhaustSound()')	   	   // Play Sound if needend
-			   
-			   if ply:GetNWInt( "stamina" ) > 0 then
-				   if ply:IsRunning() then
-					   if not ply:HasStaminaCooldown() then
-						   local skill = math.Clamp( (ply:GetSkill( "Ausdauer" ) or 0)/100, 0, 100 )
-						   ply:SetNWInt( "stamina", ply:GetNWInt( "stamina" ) - (SETTINGS.STAMINA_REFILL_DRAIN - (SETTINGS.STAMINA_REFILL_DRAIN*skill)) )
-					   end
-				   end
-			   else
-				   ply:SetRunSpeed( ply:GetWalkSpeed() )
-			   end
-			
+		if IsValid( ply ) then
+		   	ply:SendLua('Stamina.PlayExhaustSound()')	   	   // Play Sound if needend
+		   
+		   	if ply:GetNWInt( "stamina" ) > 0 then
+			   	if ply:IsRunning() then
+				   	if not ply:HasStaminaCooldown() then
+					  	local skill = math.Clamp( (ply:GetSkill( "Ausdauer" ) or 0)/100, 0, 100 )
+					   	ply:SetNWInt( "stamina", ply:GetNWInt( "stamina" ) - (SETTINGS.STAMINA_REFILL_DRAIN - (SETTINGS.STAMINA_REFILL_DRAIN*skill)) )
+				   	end
+			   	end
+		   	else
+			   	ply:SetRunSpeed( ply:GetWalkSpeed() )
+		   	end
+		
 			ply.cuffed = ply.cuffed or false
-			   if ply:GetNWInt( "stamina" ) >= 15 && !(ply.cuffed) then
+		   	if ply:GetNWInt( "stamina" ) >= 15 && !(ply.cuffed) then
 				local speed = 0
 				if ply:HasBrokenLegs() > 0 then speed = SETTINGS.CrippledRunSpeed else speed = SETTINGS.RunSpeed end
-				
-				   ply:SetRunSpeed( speed )
-				   ply:StaminaCooldown( false )
-			   end
-
-			   if ply:GetNWInt( "stamina" ) <= 0 then
-				   ply:StaminaCooldown( true )
-			   end
 			
-			   if not ply:IsRunning() or ply:HasStaminaCooldown() then
-				   if ply:GetNWInt( "stamina" ) < 100 then
-					   ply:SetNWInt( "stamina", math.Clamp( ply:GetNWInt( "stamina" ) + SETTINGS.STAMINA_REFILL_REFILLAMD, 0, 100 ) )
+			   	ply:SetRunSpeed( speed )
+			  	ply:StaminaCooldown( false )
+		   	end
+
+		   	if ply:GetNWInt( "stamina" ) <= 0 then
+			   	ply:StaminaCooldown( true )
+		   	end
+		
+		   	if not ply:IsRunning() or ply:HasStaminaCooldown() then
+			   	if ply:GetNWInt( "stamina" ) < 100 then
+				   	ply:SetNWInt( "stamina", math.Clamp( ply:GetNWInt( "stamina" ) + SETTINGS.STAMINA_REFILL_REFILLAMD, 0, 100 ) )
+				   
+				   	// Stamina EXP
+				   	ply.stamina_exp = ply.stamina_exp or 0
+				   	ply.stamina_exp = ply.stamina_exp + SETTINGS.STAMINA_REFILL_REFILLAMD
+				   	if ply.stamina_exp >= SETTINGS.STAMINA_EXP_GIVE_AT then
+					   	local exp = ply.stamina_exp / SETTINGS.STAMINA_EXP
+					   	ply.stamina_exp = 0
 					   
-					   // Stamina EXP
-					   ply.stamina_exp = ply.stamina_exp or 0
-					   ply.stamina_exp = ply.stamina_exp + SETTINGS.STAMINA_REFILL_REFILLAMD
-					   if ply.stamina_exp >= SETTINGS.STAMINA_EXP_GIVE_AT then
-						   local exp = ply.stamina_exp / SETTINGS.STAMINA_EXP
-						   ply.stamina_exp = 0
-						   
-						   ply:SetSkill( "Ausdauer", exp, true )
-					   end
-				   end
-			   end
-		   else
-			   timer.Destroy( ply:UniqueID() .. "_staminacheck" )
-			   return
-		   end
+					   	ply:SetSkill( "Ausdauer", exp, true )
+				   	end
+			   	end
+		   	end
+	   	else
+		   	timer.Destroy( ply:UniqueID() .. "_staminacheck" )
+		   	return
+	   	end
 	end
 end
 
 if CLIENT then
 	function Stamina.PlayExhaustSound()
-		if Stamina then
+		if Stamina and IsValid(LocalPlayer()) then
 			local stamina = LocalPlayer():GetNWInt( "stamina" )
 			if stamina <= 40 then
 				if !Stamina.Sound then
