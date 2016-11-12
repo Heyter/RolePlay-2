022b968bff693dfbdf8b2d87bc8fec2814ef4d92
CaMoTraX <camotrax@web.de>
2016-11-13 00:04:13 +0100

    Added Police Models

---
+++ addons/UK Police Playermodels/lua/autorun/uk_police.lua
@@ -0,0 +1,30 @@
+/*
+	Addon by Voikanaa	
+*/
+
+player_manager.AddValidModel( "UK_Police_01", 		"models/player/uk_police/uk_police_01.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_01", 	"models/player/uk_police/uk_police_01.mdl" );
+
+player_manager.AddValidModel( "UK_Police_02", 		"models/player/uk_police/uk_police_02.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_02", 	"models/player/uk_police/uk_police_02.mdl" );
+
+player_manager.AddValidModel( "UK_Police_03", 		"models/player/uk_police/uk_police_03.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_03", 	"models/player/uk_police/uk_police_03.mdl" );
+
+player_manager.AddValidModel( "UK_Police_04", 		"models/player/uk_police/uk_police_04.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_04", 	"models/player/uk_police/uk_police_04.mdl" );
+
+player_manager.AddValidModel( "UK_Police_05", 		"models/player/uk_police/uk_police_05.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_05", 	"models/player/uk_police/uk_police_05.mdl" );
+
+player_manager.AddValidModel( "UK_Police_06", 		"models/player/uk_police/uk_police_06.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_06", 	"models/player/uk_police/uk_police_06.mdl" );
+
+player_manager.AddValidModel( "UK_Police_07", 		"models/player/uk_police/uk_police_07.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_07", 	"models/player/uk_police/uk_police_07.mdl" );
+
+player_manager.AddValidModel( "UK_Police_08", 		"models/player/uk_police/uk_police_08.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_08", 	"models/player/uk_police/uk_police_08.mdl" );
+
+player_manager.AddValidModel( "UK_Police_09", 		"models/player/uk_police/uk_police_09.mdl" );
+list.Set( "PlayerOptionsModel", "UK_Police_09", 	"models/player/uk_police/uk_police_09.mdl" );
