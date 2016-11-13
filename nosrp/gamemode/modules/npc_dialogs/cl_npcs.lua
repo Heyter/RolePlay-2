7a4887d4190bc6231657843b5ed58f103d05ca1c
Jakob Müller <j.mueller@outlook.de>
2016-11-13 19:39:47 +0100

    Fix

--- nosrp/gamemode/modules/npc_dialogs/cl_npcs.lua
+++ nosrp/gamemode/modules/npc_dialogs/cl_npcs.lua
@@ -51,10 +51,9 @@ local function openDialog( dialog, npc )
 	modelPanel:SetModel( npc:GetModel( ) )
 	modelPanel:SetWide( 100 )
 	modelPanel:SetAnimated( false )
-	
 	local max, min = modelPanel:GetEntity():GetRenderBounds()
-    modelPanel:SetCamPos( Vector( 0.55, 0.55, 0.55 ) * min:Distance( max ) )
-	modelPanel:SetLookAt( ( min + max ) / 2 )
+    --modelPanel:SetCamPos( Vector( 0.55, 0.55, 0.55 ) * min:Distance( max ) )
+	--modelPanel:SetLookAt( ( min + max ) / 2 )
 	function modelPanel:LayoutEntity( )
 		
 		--modelPanel:SetCamPos( Vector( 50, -10, 60 ) )
