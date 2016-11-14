2a30e3350811223479cd743e7f5138e33d490758
Jakob Müller <j.mueller@outlook.de>
2016-11-14 17:00:21 +0100

    Added organization

---
+++ addons/org/lua/orgs/sh_orglang.lua
@@ -0,0 +1,145 @@
+ORGS_Lang = {}
+
+ORGS_Lang.orgleft = "has left the ".. ORGS_Config.addonName .."!" 
+ORGS_Lang.orgleft1 = "You have left your ".. ORGS_Config.addonName .."!"
+
+ORGS_Lang.havenoorg = "You have no ".. ORGS_Config.addonName .."!"
+
+ORGS_Lang.neworg = "Congratulations! Your new ".. ORGS_Config.addonName .." has been created!" 
+ORGS_Lang.neworg1 ="Type !orghelp to learn about how to manage your new ".. ORGS_Config.addonName .."."
+
+ORGS_Lang.cantafford = "Get the money first, you can not afford this."
+
+ORGS_Lang.inuse = "This ".. ORGS_Config.addonName .." name is already in use."
+
+ORGS_Lang.alreadyin = "You are already in a ".. ORGS_Config.addonName .."." 
+ORGS_Lang.alreadyin1 = "Leave your old one and try to create a new one again!" 
+ORGS_Lang.alreadyin2 = "This player is already in your ".. ORGS_Config.addonName .."!"
+
+ORGS_Lang.sent = "Invite sent!"
+ORGS_Lang.cant = "You can not invite yourself!" 
+ORGS_Lang.leader = "You have to be the ".. ORGS_Config.addonName .." leader to do this!"
+
+ORGS_Lang.leaving = "Leaving the old ".. ORGS_Config.addonName ..""
+
+ORGS_Lang.accept = "You have chosen to accept the invitation, welcome to your new ".. ORGS_Config.addonName .."!"
+ORGS_Lang.joined = " has been joined to the ".. ORGS_Config.addonName .."!"
+
+ORGS_Lang.notinvited = "No one invited you."
+ORGS_Lang.deny = "You have chosen to deny the invitation."
+
+ORGS_Lang.plnomoney = "You do not have enough money!"
+ORGS_Lang.orgnomoney = "There is no money in this amount in the ".. ORGS_Config.addonName .." bank."
+ORGS_Lang.validnum = "You must to enter a valid number!"
+
+ORGS_Lang.nopermission = "Your rank does not have the permission to do this!"
+
+ORGS_Lang.motdchange = "The MOTD has been changed!"
+
+ORGS_Lang.offine = "Member is offline"
+ORGS_Lang.memberkicked = " has been kicked from the ".. ORGS_Config.addonName .."!"
+ORGS_Lang.youkicked = "You have been kicked from the ".. ORGS_Config.addonName .."!"
+ORGS_Lang.cantkickowner = "You can not kick the owner of the " .. ORGS_Config.addonName .. "!"
+
+ORGS_Lang.cantrankowner = "You can not set the rank to the " .. ORGS_Config.addonName .. " owner!"
+
+ORGS_Lang.rankcreate = "Rank created!"
+ORGS_Lang.rankupdate = "Rank updated!"
+ORGS_Lang.rankdelete = "The rank has been deleted!"
+
+ORGS_Lang.rankowner = "Owner" 
+ORGS_Lang.ranknewmember = "New member"
+
+ORGS_Lang.orghelp1 = ORGS_Config.addonName .." Commands"
+ORGS_Lang.orghelp2 = "!orgmenu - To open the ".. ORGS_Config.addonName .." menu"
+ORGS_Lang.orghelp3 = "!orgc [Text] - To talk in the ".. ORGS_Config.addonName .." chat"
+
+ORGS_Lang.dashboard = "Dashboard" 
+ORGS_Lang.members = "Members" 
+ORGS_Lang.membername = "Member Name" 
+ORGS_Lang.memberrank = "Member Rank" 
+ORGS_Lang.isonline = "Is Currently Online" 
+ORGS_Lang.lastseen = "Last Seen" 
+ORGS_Lang.yes = "Yes" 
+ORGS_Lang.no = "No"
+
+ORGS_Lang.updaterank = "Change Rank"
+
+ORGS_Lang.options = "Options" 
+ORGS_Lang.optionholo = "Holo" 
+ORGS_Lang.optionchat = "Chat"
+
+ORGS_Lang.optionleave = "Leave" 
+ORGS_Lang.warning = "Warning!"
+ORGS_Lang.leave = "Are you sure that you want to leave the the "..ORGS_Config.addonName.."?"
+
+ORGS_Lang.bank = "Bank" 
+ORGS_Lang.bankwelcome = "Welcome to the bank!" 
+ORGS_Lang.bankbalance = "Balance: " 
+ORGS_Lang.bankhowmuchdeposit = [[How much do you want to deposit?]] 
+ORGS_Lang.bankhowmuchwithdraw = [[How much do you want to withdraw?]] 
+ORGS_Lang.bankdeposit = "Deposit" 
+ORGS_Lang.bankwithdraw = "Withdraw" 
+ORGS_Lang.bankback = "Back"
+
+ORGS_Lang.manage = "Manage" 
+ORGS_Lang.rankname = "Rank name.."
+
+ORGS_Lang.flags = "Rank Flags - For example - a,b,c,d" 
+ORGS_Lang.permissions = "Permission" 
+ORGS_Lang.flagsdeposit = "Deposit Money" 
+ORGS_Lang.flagswithdraw = "Withdraw Money" 
+ORGS_Lang.flagsinvite = "Invite Players"
+ORGS_Lang.flagsmotd = "Set MoTD" 
+ORGS_Lang.flagskick = "Kick Members" 
+ORGS_Lang.flagsranks = "Change Ranks" 
+ORGS_Lang.flagsranks1 = "Manage Ranks"
+
+ORGS_Lang.createrank = "Create Rank!" 
+ORGS_Lang.notallowed = "This name is not allowed!" 
+ORGS_Lang.createranks = "Create Ranks" 
+ORGS_Lang.selectrank = "Select rank!" 
+ORGS_Lang.editrank = "Edit Rank!" 
+ORGS_Lang.deleterank = "Delete Rank!"
+
+ORGS_Lang.editranks = "Edit Ranks"
+
+ORGS_Lang.chooserank = "Choose Rank..."
+
+ORGS_Lang.changemotd = "Change MoTD!"
+
+ORGS_Lang.limit = "You passed the characters limit! (the limit is 350)"
+
+ORGS_Lang.chooseplayer = "Choose Player" 
+ORGS_Lang.needchooseplayer = "You need to choose a player!"
+
+ORGS_Lang.inviteplayer = "Invite Player!" 
+ORGS_Lang.invite = "Invite"
+
+ORGS_Lang.invited = "You have been invited to ".. 
+ORGS_Config.addonName .."!"
+
+ORGS_Lang.acceptinvite = "Accept Invite" 
+ORGS_Lang.denyinvite = "Deny Invite"
+
+ORGS_Lang.holoon = "Members Halo Effect in now ON. (Affected only on the online members)" 
+ORGS_Lang.holooff = "Members Halo Effect in now OFF. (Affected only on the online members)"
+
+ORGS_Lang.chaton = ORGS_Config.addonName .. " Chat is visible from now and on." 
+ORGS_Lang.chatoff = ORGS_Config.addonName .. " Chat is invisible from now and on."
+
+ORGS_Lang.meetexplain = "Here you can create meetings with the organisation. Just stand where you want the meeting to be and fill the following entries and the members will get an alert about the meeting." 
+ORGS_Lang.meetexplain1 = "A short description about the meeting" 
+ORGS_Lang.meetexplain2 = "Where is the meeting? describe the place (example: Apartment 1)" 
+ORGS_Lang.meetcreate = "Create a Meeting!" 
+ORGS_Lang.meetset = "has been set an ".. ORGS_Config.addonName .." meeting at"
+
+--[[ NPC ]]--
+
+ORGS_Lang.npcLine = "Hey, here you can create new organisations or leave your exist one. Creating a new ".. ORGS_Config.addonName .." will cost you ".. ORGS_Config.createPrice .."$. Choose down below what you are willing to do.\nGood luck!" 
+ORGS_Lang.npcleave = "I want to leave my ".. ORGS_Config.addonName .."." 
+ORGS_Lang.npcnew = "I want to create a new ".. ORGS_Config.addonName ..". (Cost ".. ORGS_Config.createPrice .."$)" 
+ORGS_Lang.npcgetmoney = "Get the money first, you can not afford this." 
+ORGS_Lang.npccallorg = "Ok, so you want to create a new ".. ORGS_Config.addonName ..", how are you calling to your ".. ORGS_Config.addonName .."?\n\nChoose the name carefully, you can't change the name again!" 
+ORGS_Lang.npcnametext = "Enter the ".. ORGS_Config.addonName .." name.." 
+ORGS_Lang.npccreate = "Create the ".. ORGS_Config.addonName .."."
