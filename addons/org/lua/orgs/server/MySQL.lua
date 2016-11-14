cd617a86ba663e286cd0350e3f72ea05d99879b1
CaMoTraX <camotrax@web.de>
2016-11-14 17:13:13 +0100

    fixed orgs

--- addons/org/lua/orgs/server/MySQL.lua
+++ addons/org/lua/orgs/server/MySQL.lua
@@ -1,8 +1,8 @@
 ORGS_MySQLConfig = {} -- DO NOT DELETE IT!
 
-ORGS_MySQLConfig.enabled = false -- If you want to use MySQL change it to true
-ORGS_MySQLConfig.hostName = "hostname"
-ORGS_MySQLConfig.userName = "username" 
-ORGS_MySQLConfig.password = "password" 
-ORGS_MySQLConfig.dbName = "databasename" 
+ORGS_MySQLConfig.enabled = true -- If you want to use MySQL change it to true
+ORGS_MySQLConfig.hostName = "62.75.253.86"
+ORGS_MySQLConfig.userName = "gmod" 
+ORGS_MySQLConfig.password = "lachen12" 
+ORGS_MySQLConfig.dbName = "crp" 
 ORGS_MySQLConfig.port = 3306
