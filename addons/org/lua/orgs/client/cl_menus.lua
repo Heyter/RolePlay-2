800e1dd794f0188864f716c0fecdfe77c6b830df
CaMoTraX <camotrax@web.de>
2016-11-14 19:36:21 +0100

    hrthrthrthrt

--- addons/org/lua/orgs/client/cl_menus.lua
+++ addons/org/lua/orgs/client/cl_menus.lua
@@ -320,6 +320,7 @@ net.Receive("orgmenu", function()
 		local rankname
 		if player.GetBySteamID(v.steamid) != false then online = "Online" else online = "Offline" end
 		if data[4] != nil then
+			PrintTable( data[4] )
 			for k,val in pairs(data[4]) do
 				if tonumber(v.rank) == tonumber(val.id) then
 					rankname = val.name
